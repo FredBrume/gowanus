51ab8b278007c24231d8491f1191d972392188e2
FredBrume <FredBrume@users.github.com>
2018-12-24 13:12:21 -0500

    Updates

---
+++ precache-manifest.1bd0471b837974b6f0b8034724901f4a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "601da4f79db46ae983a291dde7097520",
+    "url": "/gowanus/static/media/img1.601da4f7.JPG"
+  },
+  {
+    "revision": "34920e1136c3c28932f5",
+    "url": "/gowanus/static/css/main.a790580d.chunk.css"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/gowanus/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "cf5928617be7d914a876",
+    "url": "/gowanus/static/js/1.cf592861.chunk.js"
+  },
+  {
+    "revision": "d1aac27733eb9610f64b",
+    "url": "/gowanus/static/js/runtime~main.d1aac277.js"
+  },
+  {
+    "revision": "5968f7b93c4fa8cd1e5507bef58bcd63",
+    "url": "/gowanus/static/media/img4.5968f7b9.JPG"
+  },
+  {
+    "revision": "b89a737f902124a35454b92737e33083",
+    "url": "/gowanus/static/media/img5.b89a737f.JPG"
+  },
+  {
+    "revision": "34920e1136c3c28932f5",
+    "url": "/gowanus/static/js/main.34920e11.chunk.js"
+  },
+  {
+    "revision": "b3ee9f4c58f09c1b07d102538423e1d9",
+    "url": "/gowanus/static/media/img2.b3ee9f4c.jpg"
+  },
+  {
+    "revision": "eac524e27208f09a3569e604a3a7fd67",
+    "url": "/gowanus/static/media/img3.eac524e2.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/gowanus/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/gowanus/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/gowanus/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "cf5928617be7d914a876",
+    "url": "/gowanus/static/css/1.c86eded5.chunk.css"
+  },
+  {
+    "revision": "59d3f323b44025a8e33115b30e256fa9",
+    "url": "/gowanus/index.html"
+  }
+];
